9d384e9716e131788c921184dab317461f8b1805
Morpheus7007 <sohamdoshi17@gmail.com>
2022-12-19 01:40:44 +0530

    day5

--- nimble-purpose-9234/src/com/MGNREGA/main/Home.java
+++ nimble-purpose-9234/src/com/MGNREGA/main/Home.java
@@ -2,6 +2,7 @@ package com.MGNREGA.main;
 
 import java.util.Scanner;
 
+import com.MGNREGA.colors.ConsoleColor;
 import com.MGNREGA.dao.BDODao;
 import com.MGNREGA.dao.BDODaoImpl;
 import com.MGNREGA.dao.GPMDao;
@@ -14,10 +15,17 @@ public class Home {
 	public static void HomePage(){
 		
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Welcome to MGNREGA");
-		System.out.println("select to login as BDO");
-		System.out.println("2. Login as GPM");
+		System.out.println();
+		System.out.println(ConsoleColor.GREEN_BOLD_BRIGHT+"** Welcome to MNREGA(The Mahatma Gandhi National Rural Employment Guarantee Act)"+ConsoleColor.RESET);
+		System.out.println(ConsoleColor.GREEN_BRIGHT+"(application version 1.5)"+ConsoleColor.RESET);
+		System.out.println();
+		System.out.println(ConsoleColor.BANANA_YELLOW_BOLD+"Select option number"+ConsoleColor.RESET);
+		System.out.println();
+		System.out.println("1. Login as BDO (Block Development Officer)");
+		System.out.println("2. Login as GPM (Gram Panchayat Member)");
+		System.out.println("3. Exit");
+		System.out.println();
+		System.out.println(ConsoleColor.BANANA_YELLOW_BOLD+"Enter option number :"+ConsoleColor.RESET);
 		int opt = sc.nextInt();
 		
 		if(opt ==1) {
@@ -34,6 +42,9 @@ public class Home {
 			} catch (GPMException e) {
 				System.out.println(e.getMessage());
 			}
+		}else if(opt ==3) {
+			sc.close();
+			System.exit(0);
 		}
 	}
 }
--- nimble-purpose-9234/src/com/MGNREGA/usecases/GPMLoginUseCases.java
+++ nimble-purpose-9234/src/com/MGNREGA/usecases/GPMLoginUseCases.java
@@ -2,6 +2,7 @@ package com.MGNREGA.usecases;
 
 import java.util.Scanner;
 
+import com.MGNREGA.colors.ConsoleColor;
 import com.MGNREGA.dao.GPMDao;
 import com.MGNREGA.dao.GPMDaoImpl;
 import com.MGNREGA.exception.EmployeeException;
@@ -14,16 +15,20 @@ public class GPMLoginUseCases {
 	public static void GPMDashbord() {
 		
 		Scanner sc = new Scanner(System.in);
-		
+		System.out.println();
+		System.out.println(ConsoleColor.BANANA_YELLOW_BOLD+"Enter option number to perform the following oprations"+ConsoleColor.RESET);
+		System.out.println();
 		System.out.println("1. Create an Employee");
 		System.out.println("2. View the details of employee");
 		System.out.println("3. Assign Employee to a Project");
 		System.out.println("4. View total number of days Employee worked in a project and also their wages.");
 		System.out.println("5. Go Back");
+		System.out.println();
+		System.out.println(ConsoleColor.BANANA_YELLOW_BOLD+"Enter Option Number :"+ConsoleColor.RESET);
 		
 		int opt = sc.nextInt();
 		while(opt < 0 || opt > 5) {
-			System.out.println("Enter vaild Option number");
+			System.out.println(ConsoleColor.BANANA_YELLOW_BOLD+"Enter vaild Option number"+ConsoleColor.RESET);
 			opt = sc.nextInt();
 		}
 		
@@ -33,33 +38,41 @@ public class GPMLoginUseCases {
 		
 		if(opt == 1) {
 			try {
+				System.out.println();
 				System.out.println(dao.CrateEmployee());
 			} catch (EmployeeException e) {
-				System.out.println(e.getMessage());
+				System.out.println();
+				System.out.println(ConsoleColor.RED_BOLD_BRIGHT+e.getMessage()+ConsoleColor.RESET);
 			} finally {
 				GPMDashbord();
 			}
 		}else if(opt == 2) {
 			try {
+				System.out.println();
 				PrintTable.printEmployeeTable(dao.ViewDetailsOfEmployee());
 			} catch (EmployeeException e) {
-				System.out.println(e.getMessage());
+				System.out.println();
+				System.out.println(ConsoleColor.RED_BOLD_BRIGHT+e.getMessage()+ConsoleColor.RESET);
 			} finally {
 				GPMDashbord();
 			}
 		}else if (opt == 3) {
 			try {
+				System.out.println();
 				System.out.println(dao.AssignProjectToEmployee());
 			} catch (EmployeeException e) {
-				System.out.println(e.getMessage());
+				System.out.println();
+				System.out.println(ConsoleColor.RED_BOLD_BRIGHT+e.getMessage()+ConsoleColor.RESET);
 			} finally {
 				GPMDashbord();
 			}
 		}else if(opt == 4) {
 			try {
-				 PrintTable.printEmployeeWagesTable(dao.ViewTotalNumberOfDaysOnProjectAndWages());
+				System.out.println();
+				PrintTable.printEmployeeWagesTable(dao.ViewTotalNumberOfDaysOnProjectAndWages());
 			} catch (EmployeeException e) {
-				System.out.println(e.getMessage());
+				System.out.println();
+				System.out.println(ConsoleColor.RED_BOLD_BRIGHT+e.getMessage()+ConsoleColor.RESET);
 			} finally {
 				GPMDashbord();
 			}
